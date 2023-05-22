5fb379e9ddfd834e2ce48ea096b649757b1928f6
Ryoh827 <rn.gjt25@gmail.com>
2023-05-22 21:57:39 +0900

    feat: Add feature to send to slack

--- src/handler.ts
+++ src/handler.ts
@@ -1,19 +1,31 @@
 import { Handler } from 'aws-lambda';
 import axios, { AxiosRequestHeaders, AxiosResponseHeaders } from 'axios';
+import * as Raindrop from './types/raindrop';
 
 export const notify: Handler = async (_event, _context) => {
-  const client = new ApiClient(process.env.RAINDROP_ACEESS_KEY || '');
-  const raindrops = await client.getAllRaindrops();
+  const client = new RaindropApiClient(process.env.RAINDROP_ACEESS_KEY || '');
+  const raindrops: Raindrop.Raindrop[] = await client.getAllRaindrops();
 
+  // filter raindrops updated yesterday
   const raindropsUpdatedYesterday = raindrops.filter((raindrop: any) => {
     const updatedAt = new Date(raindrop.lastUpdate);
     const yesterday = new Date();
     yesterday.setDate(yesterday.getDate() - 1);
     return updatedAt > yesterday;
   });
+
+  const text = [
+    `昨日のRaindrop.ioの更新数: ${raindropsUpdatedYesterday.length}`,
+  ];
+  const payload = {
+    text: text.join('\n'),
+  };
+
+  // send to slack
+  await axios.post(process.env.SLACK_WEBHOOK_URL || '', payload);
 };
 
-class ApiClient {
+class RaindropApiClient {
   private readonly header;
   private readonly getRaindropsUri: string =
     'https://api.raindrop.io/rest/v1/raindrops/';
@@ -26,19 +38,37 @@ class ApiClient {
     };
   }
 
-  private async getAllRootCollections() {
+  /**
+   * get all root collections
+   * @returns {Promise<Raindrop.RaindropCollection[]>}
+   */
+  private async getAllRootCollections(): Promise<
+    Raindrop.RaindropCollection[]
+  > {
     const reqUri: string = 'https://api.raindrop.io/rest/v1/collections/';
     const response = await axios.get(reqUri, { headers: this.header });
     return response.data.items;
   }
 
-  private async getRaindrops(collectionId: number) {
+  /**
+   * get raindrops by collection id
+   * @param {number} collectionId
+   * @returns {Promise<Raindrop.Raindrop[]>}
+   */
+  private async getRaindrops(
+    collectionId: number
+  ): Promise<Raindrop.Raindrop[]> {
     const reqUri: string = `${this.getRaindropsUri}${collectionId}`;
     const response = await axios.get(reqUri, { headers: this.header });
     return response.data.items;
   }
 
-  async getAllRaindrops() {
+  /**
+   * get all raindrops
+   * @returns {Promise<Raindrop.Raindrop[]>}
+   */
+
+  async getAllRaindrops(): Promise<Raindrop.Raindrop[]> {
     const rootCollections = await this.getAllRootCollections();
     const raindrops = await Promise.all(
       rootCollections.map(async (collection: any) => {
---
+++ src/types/raindrop.d.ts
@@ -0,0 +1,27 @@
+export interface Raindrop {
+  _id: number;
+  link: string;
+  title: string;
+  excerpt: string;
+  tags: string[];
+  cover: {
+    _id: number;
+    type: string;
+    url: string;
+  };
+  domain: string;
+}
+
+export interface RaindropCollection {
+  _id: number;
+  title: string;
+  parent: number;
+  children: number[];
+  cover: {
+    _id: number;
+    type: string;
+    url: string;
+  };
+  public: boolean;
+  items: Raindrop[];
+}
